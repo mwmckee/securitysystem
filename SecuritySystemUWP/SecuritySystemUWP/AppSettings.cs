95969fe6e9c6401b876241101dca84840fde7919
Matt McKee <matt_mckee@outlook.com>
2016-02-05 13:21:55 -0800

    remove uneeded variables

--- SecuritySystemUWP/SecuritySystemUWP/AppSettings.cs
+++ SecuritySystemUWP/SecuritySystemUWP/AppSettings.cs
@@ -68,11 +68,7 @@ namespace SecuritySystemUWP
         public const string AzureConnectionSettings = "DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}";
         public const string OneDriveRedirectUrl = "https://login.live.com/oauth20_desktop.srf";
         public const string OneDriveLoginUrl = "https://login.live.com/oauth20_authorize.srf?client_id={0}&scope={1}&response_type=code&redirect_uri={2}";
-        public const string OneDriveLogoutUrl = "https://login.live.com/oauth20_logout.srf?client_id={0}&redirect_uri={1}";
         public const string OneDriveScope = "wl.offline_access onedrive.readwrite";
-        public const string OneDriveRootUrl = "https://api.onedrive.com/v1.0/drive/root:";
-        public const string OneDriveTokenUrl = "https://login.live.com/oauth20_token.srf";
-        public const string OneDriveTokenContent = "client_id={0}&redirect_uri={1}&client_secret={2}&{3}={4}&grant_type={5}";
         public const string ImageNameFormat = "{0}/{1}.jpg";
 
         public static readonly StorageFolder SettingsFolder = ApplicationData.Current.LocalFolder;
